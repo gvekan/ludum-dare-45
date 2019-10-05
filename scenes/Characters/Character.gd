ccb49b1876897a9d157a7b41645576bbee223998
Gustav <gusan092@student.liu.se>
2019-10-05 20:23:24 +0200

    Fixed horizontal facing of player when still

--- scenes/Characters/Character.gd
+++ scenes/Characters/Character.gd
@@ -5,16 +5,19 @@ export var speed = 200
 
 
 var is_f = true
+var is_l = true
 
 func move(x, y): 
 	var animation = "still_f"
 	var is_run = y != 0 or x != 0
 	if y != 0:
 		is_f = y > 0
+	if x != 0:
+		is_l = x < 0
 	if is_f:
-		get_node("Sprite").set_flip_h(x > 0)
+		get_node("Sprite").set_flip_h(!is_l)
 	else:
-		get_node("Sprite").set_flip_h(x < 0)
+		get_node("Sprite").set_flip_h(is_l)
 	if is_run:
 		if is_f:
 			animation = "run_f"
