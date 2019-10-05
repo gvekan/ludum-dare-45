c3ac9108e6f910c8ad9670a2cfe8bcfd357d6cfd
Gustav <gusan092@student.liu.se>
2019-10-05 20:13:18 +0200

    CHanged player sprite and animations

--- scenes/Characters/Character.gd
+++ scenes/Characters/Character.gd
@@ -2,21 +2,29 @@ extends KinematicBody2D
 
 export var speed = 200
 
-var animation = ""
 
-func move(x, y):
-	if x > 0:
-		animation = "right"
-	elif x < 0:
-		animation = "left"
-	elif y > 0:
-		animation = "down"
-	elif y < 0:
-		animation = "up"
+
+var is_f = true
+
+func move(x, y): 
+	var animation = "still_f"
+	var is_run = y != 0 or x != 0
+	if y != 0:
+		is_f = y > 0
+	if is_f:
+		get_node("Sprite").set_flip_h(x > 0)
 	else:
-		$AnimationPlayer.stop()
-		return
+		get_node("Sprite").set_flip_h(x < 0)
+	if is_run:
+		if is_f:
+			animation = "run_f"
+		else:
+			animation = "run_b"
+	elif not is_f:
+		animation = "still_b"
+
 	$AnimationPlayer.play(animation)
-	var velocity = Vector2(x, y).normalized() * speed
+	if is_run:
+		var velocity = Vector2(x, y).normalized() * speed
+		move_and_slide(velocity)
 	
-	move_and_slide(velocity)
