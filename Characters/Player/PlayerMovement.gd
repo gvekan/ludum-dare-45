888b9447a2bd6c887062d39bd7a453732d467d6f
Gustav <gusan092@student.liu.se>
2019-10-05 12:47:18 +0200

    Best movement ever

--- Characters/Player/PlayerMovement.gd
+++ Characters/Player/PlayerMovement.gd
@@ -1,20 +1,49 @@
 extends KinematicBody2D
 
-const SPEED = 200
+export var speed = 200
 
-var motion = Vector2()
+
+
+
+var last_horizontal_input = ""
+var last_vertical_input = ""
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	motion.x = 0
-	motion.y = 0
-	if Input.is_action_pressed("ui_left"):
-		motion.x = -SPEED
-	if Input.is_action_pressed("ui_right"):
-		motion.x = SPEED
-	if Input.is_action_pressed("ui_up"):
-		motion.y = -SPEED
-	if Input.is_action_pressed("ui_down"):
-		motion.y = SPEED
+	var motion = Vector2(0, 0)
+	if Input.is_action_just_pressed("ui_left"):
+		last_horizontal_input = "ui_left"
+	elif Input.is_action_just_pressed("ui_right"):
+		last_horizontal_input = "ui_right"
+	elif not Input.is_action_pressed(last_horizontal_input):
+		if Input.is_action_pressed("ui_left"):
+			last_horizontal_input = "ui_left"
+		elif Input.is_action_pressed("ui_right"):
+			last_horizontal_input = "ui_right"
+		else:
+			last_horizontal_input = ""
+	
+	if last_horizontal_input == "ui_left":
+		motion.x = -speed
+	elif last_horizontal_input == "ui_right":
+		motion.x = speed
+		
+
+	if Input.is_action_just_pressed("ui_up"):
+		last_vertical_input = "ui_up"
+	elif Input.is_action_just_pressed("ui_down"):
+		last_vertical_input = "ui_down"
+	elif not Input.is_action_pressed(last_vertical_input):
+		if Input.is_action_pressed("ui_up"):
+			last_vertical_input = "ui_up"
+		elif Input.is_action_pressed("ui_down"):
+			last_vertical_input = "ui_down"
+		else:
+			last_vertical_input = ""
+	
+	if last_vertical_input == "ui_up":
+		motion.y = -speed
+	elif last_vertical_input == "ui_down":
+		motion.y = speed
 	
 	move_and_slide(motion)
