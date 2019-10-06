4bad5f20086dfd0a14a450370993fa3a6a6d0602
Gustav <gusan092@student.liu.se>
2019-10-06 16:20:12 +0200

    Created world hud

--- scenes/World.gd
+++ scenes/World.gd
@@ -14,3 +14,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Player_died():
+	pass # Replace with function body.
